9061beedb6b6cab1dc2f918691fbbeaef4f5253b
devilio07 <maxdevilio@gmail.com>
2021-06-17 01:38:31 +0530

    Nav bar and content theming with JS

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const body = document.body;
 
 const themeMap = {
     dark: "light",
-    light: "solar",
-    solar: "dark"
+    light: "sun",
+    sun: "dark"
 };
 
+body.className="";
+const theme = localStorage.getItem('theme');
+body.classList.add(theme);
 
 
 document.getElementById('themeButton').onclick = () => {
-    const currTheme = body.classList;
+    const currTheme = body.classList[0];
     const nextTheme = themeMap[currTheme];
 
     body.classList.replace(currTheme, nextTheme);
+    localStorage.setItem('theme', nextTheme);
 };
