f39cf3e29e939859bf2d3974ce7ebfe8a675e314
devilio07 <maxdevilio@gmail.com>
2021-06-17 04:05:18 +0530

    Will resolve later

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
-const body = document.body;
-
 const themeMap = {
     dark: "light",
     light: "sun",
     sun: "dark"
 };
+
 const theme = localStorage.getItem('theme');
-body.classList.remove('light');
-body.classList.add(theme);
+
+document.body.classList.replace(document.body.classList, theme);
 
 
 document.getElementById('themeButton').onclick = () => {
-    const currTheme = body.classList[0];
+    const currTheme = document.body.classList[0];
     const nextTheme = themeMap[currTheme];
 
-    body.classList.replace(currTheme, nextTheme);
+    document.body.classList.replace(currTheme, nextTheme);
     localStorage.setItem('theme', nextTheme);
 };
+
+console.log("It's working");
