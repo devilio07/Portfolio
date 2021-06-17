29d8ab438d9e06105d08b372875c242ed747f203
devilio07 <maxdevilio@gmail.com>
2021-06-17 20:38:28 +0530

    Quality of life changes.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 const themeMap = {
-    dark: "light",
-    light: "sun",
-    sun: "dark"
+    light: "dark",
+    dark: "sun",
+    sun: "light"
 };
 
 const theme = localStorage.getItem('theme');
 if (theme !== 'undefined') {
     document.body.classList.replace(document.body.classList, theme);
+    document.getElementById('Theme-changer').innerHTML = 'Toggle ' + themeMap[theme];
     console.log(theme)
 }
 else {
     document.body.classList.add('light');
+    document.getElementById('Theme-changer').innerHTML= 'Toggle ' + themeMap['light'];
+
 }
 
 document.getElementById('themeButton').onclick = () => {
@@ -18,6 +21,7 @@ document.getElementById('themeButton').onclick = () => {
     const nextTheme = themeMap[currTheme];
 
     document.body.classList.replace(currTheme, nextTheme);
+    document.getElementById('Theme-changer').innerHTML= 'Toggle ' + themeMap[nextTheme];
     localStorage.setItem('theme', nextTheme);
 };
 
