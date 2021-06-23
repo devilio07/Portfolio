9cdd487ac009a71469bbbf43fe1326ec839f98ea
devilio07 <maxdevilio@gmail.com>
2021-06-24 04:30:10 +0530

    Improved bg slides,styles added, theme support

--- js/app.js
+++ js/app.js
@@ -7,12 +7,14 @@ const themeMap = {
 const theme = localStorage.getItem('theme');
 if (theme !== 'undefined') {
     document.body.classList.replace(document.body.classList, theme);
+    document.getElementById('About').className='bg-'+theme;
     document.getElementById('Theme-changer').innerHTML = 'Toggle ' + themeMap[theme];
     console.log(theme)
 }
 else {
     document.body.classList.add('light');
     document.getElementById('Theme-changer').innerHTML= 'Toggle ' + themeMap['light'];
+    document.getElementById('About').classList.replace();
 
 }
 
@@ -22,6 +24,7 @@ document.getElementById('themeButton').onclick = () => {
 
     document.body.classList.replace(currTheme, nextTheme);
     document.getElementById('Theme-changer').innerHTML= 'Toggle ' + themeMap[nextTheme];
+    document.getElementById('About').classList.replace(('bg-'+currTheme),('bg-'+nextTheme))
     localStorage.setItem('theme', nextTheme);
 };
 
